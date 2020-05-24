6dd3e136fb718deebf606fc5f6a55c8ea6539052
Arthur Gonçalves Gomes Júnior <arthurgon@gmail.com>
2020-05-24 15:56:30 -0300

    Configuração de senhas com o BCrypt

--- src/main/java/com/telefonica/api/ConfigurationParametersApplication.java
+++ src/main/java/com/telefonica/api/ConfigurationParametersApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.telefonica.api.utils.PasswordUtils;
+
 @SpringBootApplication
 public class ConfigurationParametersApplication {
 
@@ -19,7 +21,17 @@ public class ConfigurationParametersApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
-			System.out.println("### Quantidade de elementos por página = " + this.qtdPorPagina);			
+					
+		    //Gera senha criptografada (hashing)
+			String senhaEncoded = PasswordUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded: " + senhaEncoded);	
+			//Gera a mesma senha criptografada (novo hashing)
+			senhaEncoded = PasswordUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded novamente: " + senhaEncoded);	
+			//Valida o hashing
+			System.out.println("Senha válida: " + PasswordUtils.senhaValida("123456", senhaEncoded));
+		
+			System.out.println("### Quantidade de elementos por página = " + this.qtdPorPagina);	
 		};
 	}
 }
